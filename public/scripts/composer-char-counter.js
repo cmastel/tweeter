adfd9dcea17d25ae96127dde539e48aa71a8961a
Chris Mastel <chris.mastel@gmail.com>
2019-11-05 20:58:59 +0000

    implemented counter to show remaining characters, and turn red when below 0

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $("textarea").on('keyup', function() {
+    let remainingChars = 140 - $( this ).val().length;
+    $( this ).parent().children( ".counter" ).text( remainingChars );
+
+    if (remainingChars < 0) {
+      $( this ).parent().children( ".counter" ).css("color", "red");
+    }
+  });
+
+});
