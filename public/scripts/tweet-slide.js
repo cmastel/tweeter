38f683e5a1a24b08972880b144e739719e4d42e9
Chris Mastel <chris.mastel@gmail.com>
2019-11-06 23:22:09 +0000

    added hover and focus to show-new-tweet button

--- public/scripts/tweet-slide.js
+++ public/scripts/tweet-slide.js
@@ -3,9 +3,18 @@ $(document).ready(function() {
   $("#show-new-tweet").click(
     function() {
       $(".new-tweet").slideDown("slow");
-      $("#show-new-tweet").css("transform", "translateY(4px)")
-      }
-   
+      $("#show-new-tweet").css("transform", "translateY(4px)");
+      $("textarea").focus();
+      }   
   );
+
+  $("#show-new-tweet").hover(
+    function() {
+      $("#show-new-tweet").css("box-shadow", "0 0 15px #ffffff");
+    },
+    function() {
+      $("#show-new-tweet").css("box-shadow", "none");
+    }
+  )
 
 });
