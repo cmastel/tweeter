90ecf85301df944eb2423ff839af987d67829cd9
Chris Mastel <chris.mastel@gmail.com>
2019-11-07 22:46:35 +0000

    move load-tweets into helper-functions

--- public/scripts/load-tweets.js
+++ public/scripts/load-tweets.js
@@ -1,30 +1,28 @@
-const loadTweets = function(last=false) {
-  if (last) {
-    $.ajax({
-      method: 'GET',
-      url: '/tweets/',
-      dataType: 'json',
-      success: (data) => {
-        const lastKey = Object.keys(data).reverse()[0];
-        const tweet = data[lastKey];
-        const $tweet = createTweetElement(tweet);
-        $('#tweets-container').prepend($tweet);
+// const loadTweets = function(last=false) {
+//   if (last) {
+//     $.ajax({
+//       method: 'GET',
+//       url: '/tweets/',
+//       dataType: 'json',
+//       success: (data) => {
+//         const lastKey = Object.keys(data).reverse()[0];
+//         const tweet = data[lastKey];
+//         const $tweet = createTweetElement(tweet);
+//         $('#tweets-container').prepend($tweet);
         
-      }
-    })
-  } else {
-    $.ajax({
-      method: 'GET',
-      url: '/tweets/',
-      dataType: 'json',
-      success: (data) => {
-        for (let tweet of data) {
-          const $tweet = createTweetElement(tweet);
-          $('#tweets-container').prepend($tweet);
-        }
-      }
-    })
-  }
-  
-  
-}
+//       }
+//     })
+//   } else {
+//     $.ajax({
+//       method: 'GET',
+//       url: '/tweets/',
+//       dataType: 'json',
+//       success: (data) => {
+//         for (let tweet of data) {
+//           const $tweet = createTweetElement(tweet);
+//           $('#tweets-container').prepend($tweet);
+//         }
+//       }
+//     })
+//   }
+// }
