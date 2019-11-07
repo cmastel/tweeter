f7b11b6c78da9112220581d8e0ef3ebae3cca6f5
Chris Mastel <chris.mastel@gmail.com>
2019-11-07 22:41:37 +0000

    move remainingChar counter into client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -9,6 +9,16 @@ $(document).ready(function() {
   loadTweets();
   // $("form").submit(newTweet(event));
 
+  // remainingChar counter
+  $("textarea").on('keyup', function() {
+    let remainingChars = 140 - $( this ).val().length;
+    $( this ).parent().children( ".counter" ).text( remainingChars );
+    if (remainingChars < 0) {
+      $( this ).parent().children( ".counter" ).css("color", "red");
+    }
+  });
+  
+  // submit new tweet
   $("form").submit(function(event) {
     event.preventDefault();
     const userInput = $( this ).serialize().slice(5);
