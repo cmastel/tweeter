9d903687d7289c2bb57edcdf09d4eb51e3ea7428
Chris Mastel <chris.mastel@gmail.com>
2019-11-07 23:05:53 +0000

    moved remainingCharCounter handler function to handler.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,15 +8,7 @@ $(document).ready(function() {
   
   loadTweets();
   // $("form").submit(newTweet(event));
-
-  // remainingChar counter
-  $("textarea").on('keyup', function() {
-    let remainingChars = 140 - $( this ).val().length;
-    $( this ).parent().children( ".counter" ).text( remainingChars );
-    if (remainingChars < 0) {
-      $( this ).parent().children( ".counter" ).css("color", "red");
-    }
-  });
+  $("textarea").on('keyup', remainingCharCounter);
   
   // submit new tweet
   $("form").submit(function(event) {
--- public/scripts/handlers.js
+++ public/scripts/handlers.js
@@ -22,3 +22,11 @@ const newTweet = function(event) {
   }
 }
 
+const remainingCharCounter = function() {
+  let remainingChars = 140 - $( this ).val().length;
+  $( this ).parent().children( ".counter" ).text( remainingChars );
+  if (remainingChars < 0) {
+    $( this ).parent().children( ".counter" ).css("color", "red");
+  }
+}
+
