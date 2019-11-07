f27e5c16dff2ec38d1ab9b001f79525526e4a7d2
Chris Mastel <chris.mastel@gmail.com>
2019-11-07 21:32:09 +0000

    adjusted loadTweets function so that it only loads the most recent tweet when a new one is created

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -25,22 +25,11 @@ $(document).ready(function() {
         data: $( this ).serialize(),
         success: () => {
           this.reset();
-          loadTweets();
+          loadTweets(true);
           $("#error-message").slideUp("medium");
         }
       })
     }
   })
-
-  // $(".tweets").hover(
-  //   function() {
-  //     $( this ).css("box-shadow", "5px 5px 5px #0C7BDC");
-  //     $( this ).find("#username").css("display", "block")
-  //     }, 
-  //   function() {
-  //     $( this ).css("box-shadow", "none");
-  //     $( this ).find("#username").css("display", "none");
-  //   }
-  // );
 
 });
--- public/scripts/load-tweets.js
+++ public/scripts/load-tweets.js
@@ -1,13 +1,30 @@
-const loadTweets = function() {
-  $.ajax({
-    method: 'GET',
-    url: '/tweets/',
-    dataType: 'json',
-    success: (data) => {
-      for (let tweet of data) {
+const loadTweets = function(last=false) {
+  if (last) {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets/',
+      dataType: 'json',
+      success: (data) => {
+        const lastKey = Object.keys(data).reverse()[0];
+        const tweet = data[lastKey];
         const $tweet = createTweetElement(tweet);
         $('#tweets-container').prepend($tweet);
+        
       }
-    }
-  })
+    })
+  } else {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets/',
+      dataType: 'json',
+      success: (data) => {
+        for (let tweet of data) {
+          const $tweet = createTweetElement(tweet);
+          $('#tweets-container').prepend($tweet);
+        }
+      }
+    })
+  }
+  
+  
 }
